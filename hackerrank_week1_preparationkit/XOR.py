0cb08e033b2f7d7177c55649c1f30026395e04c5
mingxuan <mingxuan8833@gmail.com>
2025-01-21 23:51:02 +0000

    added XOR

---
+++ hackerrank_week1_preparationkit/XOR.py
@@ -0,0 +1,13 @@
+def strings_xor(s, t):
+    res = ""
+    for i in range(len(s)):
+        if s[i] <= t[i]:
+            res += '1';
+        else:
+            res += '0';
+
+    return res
+
+s = input()
+t = input()
+print(strings_xor(s, t))
