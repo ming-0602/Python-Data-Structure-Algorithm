a57c9b882b4dd879556a7dd47cb91b145d3635b9
mingxuan <mingxuan8833@gmail.com>
2025-02-07 22:09:30 +0000

    added correct gridchallenge

--- hackerrank_week2_preparationkit/GridChallenge.py
+++ hackerrank_week2_preparationkit/GridChallenge.py
@@ -2,19 +2,24 @@ def gridChallenge(grid):
     # Write your code here
     # sort the array element
     # compare the the a[0] < a[1]
-    for i in grid:
-        temchararr = list(i)
-        # print(temchararr.sort())
-        sortedtemchar = sorted(temchararr)
-        print(sortedtemchar)
-        index = 0
-        while (index < sortedtemchar.size()):
-            if (sortedtemchar[index + 1] - sortedtemchar[index] != 1):
+    for i in range(len(grid)):
+        sorted_row = sorted(grid[i])  # Sort the row
+        grid[i] = ''.join(sorted_row)  # Replace the row with the sorted one
+
+        # Check each column for alphabetical order
+    col_count = len(grid[0])  # Number of columns
+    row_count = len(grid)  # Number of rows
+
+    for col in range(col_count):  # Loop through each column
+        for row in range(row_count - 1):  # Compare each row with the one below it
+            if grid[row][col] > grid[row + 1][col]:  # If the column is not sorted
                 return "NO"
-            index += 1
 
     return "YES"
 
 grid = ['ebacd', 'fghij', 'olmkn', 'trpqs', 'xywuv']
 
 gridChallenge(grid)
+
+grid = ['abc', 'lmp', 'qrt']
+print(gridChallenge(grid))
