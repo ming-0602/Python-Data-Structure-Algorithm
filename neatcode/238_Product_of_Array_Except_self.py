c41ded761e9afefb6dc743ed5ca6d30454d99bbc
mingxuan <mingxuan8833@gmail.com>
2025-02-19 22:59:48 +0000

    added 238_Product_of_Array_Except_self

--- neatcode/238_Product_of_Array_Except_self.py
+++ neatcode/238_Product_of_Array_Except_self.py
@@ -1,5 +1,27 @@
-def productExceptSelf(self, nums):
+def productExceptSelf(nums):
     """
     :type nums: List[int]
     :rtype: List[int]
     """
+    result = []
+    for i in range(len(nums)) :
+        tem_array = nums.copy()
+        tem_array.pop(i)
+        tem_value = 0
+        index = 0
+        for j in range(len(tem_array)):
+            if index == 0:
+                tem_value = tem_array[j]
+                index += 1
+            else:
+                tem_value = tem_value * tem_array[j]
+                index += 1
+
+        result.append(tem_value)
+
+    print(result)
+
+
+
+nums = [1,2,3,4]
+productExceptSelf(nums)
